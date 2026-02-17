c469b0d1265700fb4e050bdb59c53111054841d4
tetsuya111 <otetsuya1729@icloud.com>
2026-02-17 19:30:33 +0900

    notion APIの最低限の機能の実装

--- modules/mecab_api/sample/test_to_hiragana.py
+++ modules/mecab_api/sample/test_to_hiragana.py
@@ -1,7 +1,10 @@
 import sys
+import structlog
 
 import requests
 import urllib.parse as up
+
+logger=structlog.getLogger()
 
 host="localhost"
 port="8000"
@@ -12,6 +15,9 @@ n=int(sys.argv[2])
 
 base_url=f"http://{host}:{port}/"
 url=up.urljoin(base_url,path)
+
+msg=f"url : {url}"
+logger.info(msg)
 
 params={
     "name":name,
